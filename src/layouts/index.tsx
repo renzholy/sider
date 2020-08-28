0d1e11be6b301dd31834eef50fffcfc2407602eb
RenzHoly <renzholy@gmail.com>
2020-08-29 02:09:54 +0800

    Update index.tsx

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { RouteComponentProps } from 'react-router'
 import { Provider } from 'react-redux'
 
 import { store } from '@/stores/index'
 
-export default (props: RouteComponentProps & { children: React.ReactNode }) => {
+export default (props: { children: React.ReactNode }) => {
   return <Provider store={store}>{props.children}</Provider>
 }

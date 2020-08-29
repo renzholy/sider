d3d93428983ffff0a37cbc84739b813a16b52aaf
RenzHoly <renzholy@gmail.com>
2020-08-29 22:28:19 +0800

    change layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,5 +4,15 @@ import { Provider } from 'react-redux'
 import { store } from '@/stores/index'
 
 export default (props: { children: React.ReactNode }) => {
-  return <Provider store={store}>{props.children}</Provider>
+  return (
+    <Provider store={store}>
+      <div
+        style={{
+          height: '100vh',
+          overflow: 'hidden',
+        }}>
+        {props.children}
+      </div>
+    </Provider>
+  )
 }
--- src/pages/keys.tsx
+++ src/pages/keys.tsx
@@ -75,7 +75,7 @@ export default () => {
   }, [setSize])
 
   return (
-    <div style={{ width: 360, height: '100vh' }}>
+    <div style={{ width: 360, height: '100%' }}>
       <div style={{ padding: 16 }}>
         <InputGroup value={match} onChange={handleMatchChange} />
       </div>

c8ddd89781a25160306ac72c1942fd9f788d306d
Zehua Ren <renzholy@gmail.com>
2020-08-31 16:36:53 +0800

    Update SetPanel.tsx

--- src/components/panel/SetPanel.tsx
+++ src/components/panel/SetPanel.tsx
@@ -1,14 +1,20 @@
 import React, { useCallback } from 'react'
-import { useSWRInfinite } from 'swr'
+import useSWR, { useSWRInfinite } from 'swr'
 import { useSelector } from 'react-redux'
 import { ListChildComponentProps } from 'react-window'
 
 import { Unpacked } from '@/utils'
 import { sscan } from '@/utils/scanner'
+import { useScanSize } from '@/hooks/useScanSize'
+import { formatNumber } from '@/utils/formatter'
+import { runCommand } from '@/utils/fetcher'
 import { SetMatchInput } from './SetMatchInput'
 import { InfiniteList } from '../pure/InfiniteList'
 import { InfiniteListItems } from '../pure/InfiniteListItems'
 import { SetItem } from './SetItem'
+import { Footer } from '../pure/Footer'
+import { TTLButton } from '../TTLButton'
+import { ReloadButton } from '../pure/ReloadButton'
 
 export function SetPanel(props: { value: string }) {
   const connection = useSelector((state) => state.keys.connection)
@@ -33,19 +39,33 @@ export function SetPanel(props: { value: string }) {
     },
     [connection, props.value, match, isPrefix],
   )
-  const { data, setSize } = useSWRInfinite(handleGetKey, sscan, {
-    revalidateOnFocus: false,
-  })
+  const { data, setSize, isValidating, revalidate } = useSWRInfinite(
+    handleGetKey,
+    sscan,
+    {
+      revalidateOnFocus: false,
+    },
+  )
   const handleLoadMoreItems = useCallback(async () => {
     await setSize((_size) => _size + 1)
   }, [setSize])
   const renderItems = useCallback(
-    // eslint-disable-next-line react/jsx-props-no-spreading
     (p: ListChildComponentProps) => (
+      // eslint-disable-next-line react/jsx-props-no-spreading
       <InfiniteListItems {...p}>{SetItem}</InfiniteListItems>
     ),
     [],
   )
+  const { data: scard, revalidate: revalidateScard } = useSWR(
+    connection ? `scard/${connection}/${props.value}` : null,
+    () => runCommand<number>(connection!, ['scard', props.value]),
+  )
+  const scanSize = useScanSize()
+  const handleReload = useCallback(async () => {
+    await setSize(0)
+    await revalidate()
+    await revalidateScard()
+  }, [setSize, revalidate, revalidateScard])
 
   return (
     <div
@@ -56,13 +76,21 @@ export function SetPanel(props: { value: string }) {
         height: '100%',
       }}>
       <SetMatchInput />
-      {data ? (
-        <div style={{ flex: 1 }}>
+      <div style={{ flex: 1 }}>
+        {data ? (
           <InfiniteList items={data} onLoadMoreItems={handleLoadMoreItems}>
             {renderItems}
           </InfiniteList>
-        </div>
-      ) : null}
+        ) : null}
+      </div>
+      <Footer>
+        <ReloadButton isLoading={isValidating} onReload={handleReload} />
+        <span>
+          {formatNumber(scanSize)}&nbsp;of&nbsp;
+          {formatNumber(scard || 0)}
+        </span>
+        <TTLButton value={props.value} />
+      </Footer>
     </div>
   )
 }

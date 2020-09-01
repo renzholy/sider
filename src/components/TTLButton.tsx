b74fd683de8d1c90be7174f1eb1c801e57d8ca8e
Zehua Ren <renzholy@gmail.com>
2020-09-01 16:58:11 +0800

    Update TTLButton.tsx

--- src/components/TTLButton.tsx
+++ src/components/TTLButton.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react'
+import React, { CSSProperties, useState, useCallback } from 'react'
 import { Button, Tooltip } from '@blueprintjs/core'
 import useSWR from 'swr'
 import { useSelector } from 'react-redux'
@@ -9,11 +9,18 @@ import { formatNumber } from '@/utils/formatter'
 
 export function TTLButton(props: { style?: CSSProperties; value: string }) {
   const connection = useSelector((state) => state.keys.connection)
+  const [refreshInterval, setRefreshInterval] = useState(0)
   const { data } = useSWR(
     connection ? `ttl/${connection}/${props.value}` : null,
     () => runCommand<number>(connection!, ['ttl', props.value]),
-    { refreshInterval: 1000 },
+    { refreshInterval, dedupingInterval: 1000 },
   )
+  const handleOpened = useCallback(() => {
+    setRefreshInterval(1000)
+  }, [])
+  const handleClosed = useCallback(() => {
+    setRefreshInterval(0)
+  }, [])
 
   if (data === undefined) {
     return null
@@ -21,6 +28,9 @@ export function TTLButton(props: { style?: CSSProperties; value: string }) {
   return (
     <div style={props.style}>
       <Tooltip
+        onOpened={handleOpened}
+        onClosed={handleClosed}
+        boundary="window"
         content={
           data < 0 ? 'Persisted' : `Expire after ${formatNumber(data)} seconds`
         }>

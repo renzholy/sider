f780f4f963cac1860462834e84a4be42ba751e54
Found Pan Tiger <renzholy@gmail.com>
2020-09-14 19:42:56 +0800

    update info page (#16)
    * update info
    
    * Update info.tsx

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import useSWR from 'swr'
 import { useSelector } from 'react-redux'
-import { Colors } from '@blueprintjs/core'
+import { Colors, H4 } from '@blueprintjs/core'
 
 import { runCommand } from '@/utils/fetcher'
 import { useIsDarkMode } from '@/hooks/use-is-dark-mode'
+import styles from './info.less'
 
 export default () => {
   const connection = useSelector((state) => state.root.connection)
@@ -12,20 +13,57 @@ export default () => {
     runCommand<string>(connection!, ['info']),
   )
   const isDarkMode = useIsDarkMode()
+  const info = useMemo(
+    () =>
+      data
+        ?.trimRight()
+        .split('\n\r\n')
+        .map((section) =>
+          section.split('\n').map((line, index) => {
+            return index === 0 ? line.substr(2) : line.split(':')
+          }),
+        ),
+    [data],
+  )
 
   return (
     <div
       style={{
-        whiteSpace: 'pre-wrap',
-        wordBreak: 'break-word',
-        overflow: 'scroll',
-        borderRadius: 4,
-        padding: 8,
-        margin: 8,
         width: '100%',
-        backgroundColor: isDarkMode ? Colors.DARK_GRAY1 : Colors.WHITE,
+        overflow: 'scroll',
+        margin: 8,
+        borderRadius: 4,
       }}>
-      {data}
+      <div
+        style={{
+          columnCount: 2,
+          columnGap: 8,
+        }}>
+        {info?.map((section) =>
+          section.length === 1 ? null : (
+            <div
+              style={{
+                breakInside: 'avoid',
+                whiteSpace: 'pre-wrap',
+                wordBreak: 'break-word',
+                borderRadius: 4,
+                padding: 8,
+                marginBottom: 8,
+                backgroundColor: isDarkMode ? Colors.DARK_GRAY1 : Colors.WHITE,
+              }}>
+              <H4>{section[0]}</H4>
+              {section.map((line) =>
+                typeof line === 'string' ? null : (
+                  <p key={line[0]} className={styles.item}>
+                    {line[0]}
+                    <span style={{ float: 'right' }}>{line[1]}</span>
+                  </p>
+                ),
+              )}
+            </div>
+          ),
+        )}
+      </div>
     </div>
   )
 }

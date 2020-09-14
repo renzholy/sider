efc7bb32c3c082b11b9f0ac27aea6b0c36bbd153
Found Pan Tiger <renzholy@gmail.com>
2020-09-14 16:09:49 +0800

    raw runCommand

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,9 +1,19 @@
 import { Connection } from '@/types'
 
+export async function runCommand<T extends string | string[]>(
+  connection: Connection,
+  command: string[],
+  raw: true,
+): Promise<T>
 export async function runCommand<T>(
   connection: Connection,
   command: string[],
-): Promise<T> {
+): Promise<T>
+export async function runCommand(
+  connection: Connection,
+  command: string[],
+  raw = false,
+) {
   const response = await fetch(`/api/runCommand?c=${command.join('_')}`, {
     method: 'POST',
     headers: {
@@ -12,9 +22,20 @@ export async function runCommand<T>(
     body: JSON.stringify({
       connection,
       command,
+      raw,
     }),
   })
   if (response.ok) {
+    if (raw) {
+      const text = await response.text()
+      if (text.startsWith('[') && text.endsWith(']')) {
+        return text
+          .substr(1, text.length - 1)
+          .split(' ')
+          .map((str) => Buffer.from(str, 'hex').toString('binary'))
+      }
+      return Buffer.from(text, 'hex').toString('binary')
+    }
     return response.json()
   }
   throw new Error(await response.text())

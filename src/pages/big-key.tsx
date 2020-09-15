2488adddded73184c8b3d9c7300bea1da74b856f
Found Pan Tiger <renzholy@gmail.com>
2020-09-15 14:54:22 +0800

    Update big-key.tsx

--- src/pages/big-key.tsx
+++ src/pages/big-key.tsx
@@ -1,14 +1,14 @@
 import { runCommand } from '@/utils/fetcher'
 import { scan2 } from '@/utils/scanner'
 import { ProgressBar } from '@blueprintjs/core'
-import React, { useRef, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
 import useAsyncEffect from 'use-async-effect'
 
 export default () => {
   const connection = useSelector((state) => state.root.connection)
   const [dbsize, setDbsize] = useState(0)
-  const totalScanned = useRef(0)
+  const [totalScanned, setTotalScanned] = useState(0)
   useAsyncEffect(
     async (isMounted) => {
       if (!connection) {
@@ -18,13 +18,14 @@ export default () => {
         await runCommand<number>(connection, ['dbsize']),
       )
       let next = '0'
+      let _totalScanned = 0
       // eslint-disable-next-line no-constant-condition
       while (true) {
         // eslint-disable-next-line no-await-in-loop
-        const scanned = await scan2(connection, next, totalScanned.current)
+        const scanned = await scan2(connection, next, _totalScanned)
         next = scanned.next
-        totalScanned.current = scanned.totalScanned
-        console.log(scanned)
+        _totalScanned = scanned.totalScanned
+        setTotalScanned(_totalScanned)
         if (scanned.next === '0' || !isMounted()) {
           break
         }
@@ -32,10 +33,14 @@ export default () => {
     },
     [connection],
   )
+  const progress = useMemo(() => (dbsize ? totalScanned / dbsize : 0), [
+    dbsize,
+    totalScanned,
+  ])
 
   return (
     <div style={{ margin: 8, flex: 1 }}>
-      <ProgressBar value={dbsize ? totalScanned.current / dbsize : 0} />
+      <ProgressBar value={progress} />
     </div>
   )
 }
